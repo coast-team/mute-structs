aae6b4fdb723ba57c920b1272339d819e09b94cc
Matthieu Nicolas <nicolasmatthieu57@gmail.com>
2017-08-10 10:56:53 +0200

    fix(identifiertuple): Add import to now externalized type SafeAny

--- src/identifiertuple.ts
+++ src/identifiertuple.ts
@@ -17,6 +17,8 @@
  *  along with Mute-structs.  If not, see <http://www.gnu.org/licenses/>.
  */
 
+import {SafeAny} from 'safe-any'
+
 import {INT_32_MIN_VALUE, INT_32_MAX_VALUE} from './identifier'
 import {Ordering} from './ordering'
 
