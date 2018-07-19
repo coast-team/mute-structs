445ead55c5fbe429191c712bebc643555f27b9d1
Matthieu Nicolas <nicolasmatthieu57@gmail.com>
2018-07-19 17:36:55 +0200

    feat(helpers): Add findPredecessor()

---
+++ src/helpers.ts
@@ -0,0 +1,17 @@
+import {Ordering} from "./ordering"
+
+export function findPredecessor<T> (list: T[], element: T, compareFn: (a: T, b: T) => Ordering): T | undefined {
+    let predecessor
+    let isOk = true
+    let i = 0
+    while (isOk) {
+        const other = list[i]
+        if (compareFn(other, element) === Ordering.Less) {
+            predecessor = other
+            i++
+        } else {
+            isOk = false
+        }
+    }
+    return predecessor
+}
