4bf4ab14666af6620900dfd73d3698b00e11b81f
Cédric Enclos <cedric.enclos@loria.fr>
2018-11-14 15:37:08 +0100

    refactor(stats): change some names and attributes visibility

--- src/stats.ts
+++ src/stats.ts
@@ -10,32 +10,30 @@ export interface BasicStats {
 }
 
 export class Stats {
-    private documentL: number
-    private totalNodeLength: number
+    readonly documentLength: number
+    readonly treeHeight: number
     private nodeNumber: number
-    private treeHeigth: number
 
-    private nodeStat: BasicStats
-    private identifierStat: BasicStats
+    private nodesStats: BasicStats
+    private identifiersStats: BasicStats
 
-    private nodeTab: number[]
-    private identifierTab: number[]
+    private nodeLengths: number[]
+    private identifierLengths: number[]
 
     constructor (rope: LogootSRopes) {
-        this.documentL = 0
-        this.treeHeigth = 0
+        this.documentLength = rope.str.length
+        this.treeHeight = rope.height
         this.nodeNumber = 0
-        this.nodeTab = []
-        this.identifierTab = []
-        this.totalNodeLength = 0
-        this.nodeStat = {
+        this.nodeLengths = []
+        this.identifierLengths = []
+        this.nodesStats = {
             max: 0,
             min: -1,
             mean: 0,
             median: 0,
             lengthRepartition: new Map<number, number>(),
         }
-        this.identifierStat = {
+        this.identifiersStats = {
             max: 0,
             min: -1,
             mean: 0,
@@ -45,13 +43,80 @@ export class Stats {
         this.compute(rope)
     }
 
+    get numberOfNodes (): number {
+        return this.nodeNumber
+    }
+
+    get maxNodeLength (): number {
+        return this.nodesStats.max
+    }
+
+    get minNodeLength (): number {
+        return this.nodesStats.min
+    }
+
+    get meanNodeLength (): number {
+        return this.nodesStats.mean
+    }
+
+    get medianNodeLength (): number {
+        return this.nodesStats.median
+    }
+
+    get repartitionNodeLength (): Map<number, number> {
+        return this.nodesStats.lengthRepartition
+    }
+
+    get repartitionNodeLengthString (): string {
+        const arr = Array.from(this.nodesStats.lengthRepartition)
+        arr.sort((a, b) => {
+            return a[0] - b[0]
+        })
+        let str = ""
+        arr.forEach((entry) => {
+            str += "(" + entry[0] + ", " + entry[1] + "), "
+        })
+        return str
+    }
+
+    get maxIdentifierLength (): number {
+        return this.identifiersStats.max
+    }
+
+    get minIdentifierLength (): number {
+        return this.identifiersStats.min
+    }
+
+    get meanIdentifierLength (): number {
+        return this.identifiersStats.mean
+    }
+
+    get medianIdentifierLength (): number {
+        return this.identifiersStats.median
+    }
+
+    get repartitionIdentifierLength (): Map<number, number> {
+        return this.identifiersStats.lengthRepartition
+    }
+
+    get repartitionIdentifierLengthString (): string {
+        const arr = Array.from(this.identifiersStats.lengthRepartition)
+        arr.sort((a, b) => {
+            return a[0] - b[0]
+        })
+        let str = ""
+        arr.forEach((entry) => {
+            str += "(" + entry[0] + ", " + entry[1] + "), "
+        })
+        return str
+    }
+
     public toString (): string {
         let str = ""
         str += "Document stats : \n"
-        str += "\t Document length : " + this.documentL + "\n"
+        str += "\t Document length : " + this.documentLength + "\n"
         str += "\t Number of nodes : " + this.numberOfNodes + "\n"
-        str += "\t Total Nodes length : " + this.totalNodeLength + "\n"
-        str += "\t Height of the tree : " + this.heightOfTree + "\n"
+        str += "\t Height of the tree : " + this.treeHeight + "\n"
         str += "\t Nodes : " + "\n"
         str += "\t\tMax length : " + this.maxNodeLength + "\n"
         str += "\t\tMin length : " + this.minNodeLength + "\n"
@@ -72,33 +137,28 @@ export class Stats {
     }
 
     private compute (rope: LogootSRopes) {
-
-        this.documentL = rope.str.length
         this.nodeNumber = this.recCompute(rope.root)
-        if (rope.root) {
-            this.treeHeigth = rope.root.height
-        }
 
-        this.nodeTab = this.nodeTab.sort((a, b) => {
+        this.nodeLengths = this.nodeLengths.sort((a, b) => {
             return a - b
         })
-        this.identifierTab = this.identifierTab.sort((a, b) => {
+        this.identifierLengths = this.identifierLengths.sort((a, b) => {
             return a - b
         })
 
-        this.nodeStat.mean /= this.nodeNumber
-        const N = this.nodeTab.length
-        this.nodeStat.median =
+        this.nodesStats.mean /= this.nodeNumber
+        const N = this.nodeLengths.length
+        this.nodesStats.median =
             N % 2 === 0
-                ? (this.nodeTab[N / 2] + this.nodeTab[N / 2 + 1]) / 2
-                : this.nodeTab[Math.ceil(N / 2)]
+                ? (this.nodeLengths[N / 2] + this.nodeLengths[N / 2 + 1]) / 2
+                : this.nodeLengths[Math.ceil(N / 2)]
 
-        this.identifierStat.mean /= this.nodeNumber
-        const M = this.identifierTab.length
-        this.identifierStat.median =
+        this.identifiersStats.mean /= this.nodeNumber
+        const M = this.identifierLengths.length
+        this.identifiersStats.median =
             M % 2 === 0
-                ? (this.identifierTab[M / 2] + this.identifierTab[M / 2 + 1]) / 2
-                : this.identifierTab[Math.ceil(M / 2)]
+                ? (this.identifierLengths[M / 2] + this.identifierLengths[M / 2 + 1]) / 2
+                : this.identifierLengths[Math.ceil(M / 2)]
     }
 
     private recCompute (rope: RopesNodes | null): number {
@@ -109,127 +169,50 @@ export class Stats {
         // node stats
 
         const nLength = rope.length
-        this.nodeTab.push(nLength)
-        this.totalNodeLength += nLength
-        this.nodeStat.max =
-            this.nodeStat.max < nLength ? nLength : this.nodeStat.max
-        this.nodeStat.min =
-            this.nodeStat.min === -1
+        this.nodeLengths.push(nLength)
+        this.nodesStats.max =
+            this.nodesStats.max < nLength ? nLength : this.nodesStats.max
+        this.nodesStats.min =
+            this.nodesStats.min === -1
                 ? nLength
-                : this.nodeStat.min > nLength
+                : this.nodesStats.min > nLength
                     ? nLength
-                    : this.nodeStat.min
-        this.nodeStat.mean += nLength
-        if (this.nodeStat.lengthRepartition.has(nLength)) {
-            const n = this.nodeStat.lengthRepartition.get(nLength)
+                    : this.nodesStats.min
+        this.nodesStats.mean += nLength
+        if (this.nodesStats.lengthRepartition.has(nLength)) {
+            const n = this.nodesStats.lengthRepartition.get(nLength)
             if (n) {
-                this.nodeStat.lengthRepartition.set(nLength, n + 1)
+                this.nodesStats.lengthRepartition.set(nLength, n + 1)
             }
         } else {
-            this.nodeStat.lengthRepartition.set(nLength, 1)
+            this.nodesStats.lengthRepartition.set(nLength, 1)
         }
 
         // identifier stats
 
         const iLength = rope.getIdBegin().length
-        this.identifierTab.push(iLength)
-        this.identifierStat.max =
-            this.identifierStat.max < iLength ? iLength : this.identifierStat.max
-        this.identifierStat.min =
-            this.identifierStat.min === -1
+        this.identifierLengths.push(iLength)
+        this.identifiersStats.max =
+            this.identifiersStats.max < iLength ? iLength : this.identifiersStats.max
+        this.identifiersStats.min =
+            this.identifiersStats.min === -1
                 ? iLength
-                : this.identifierStat.min > iLength
+                : this.identifiersStats.min > iLength
                     ? iLength
-                    : this.identifierStat.min
-        this.identifierStat.mean += iLength
+                    : this.identifiersStats.min
+        this.identifiersStats.mean += iLength
         if (
-            this.identifierStat.lengthRepartition.has(iLength) &&
-            this.identifierStat.lengthRepartition
+            this.identifiersStats.lengthRepartition.has(iLength) &&
+            this.identifiersStats.lengthRepartition
         ) {
-            const n = this.identifierStat.lengthRepartition.get(iLength)
+            const n = this.identifiersStats.lengthRepartition.get(iLength)
             if (n) {
-                this.identifierStat.lengthRepartition.set(iLength, n + 1)
+                this.identifiersStats.lengthRepartition.set(iLength, n + 1)
             }
         } else {
-            this.identifierStat.lengthRepartition.set(iLength, 1)
+            this.identifiersStats.lengthRepartition.set(iLength, 1)
         }
 
         return this.recCompute(rope.left) + 1 + this.recCompute(rope.right)
-    }
-
-    get documentLength (): number {
-        return this.documentL
-    }
-
-    get numberOfNodes (): number {
-        return this.nodeNumber
-    }
-
-    get heightOfTree (): number {
-        return this.treeHeigth
-    }
-
-    get maxNodeLength (): number {
-        return this.nodeStat.max
-    }
-
-    get minNodeLength (): number {
-        return this.nodeStat.min
-    }
-
-    get meanNodeLength (): number {
-        return this.nodeStat.mean
-    }
-
-    get medianNodeLength (): number {
-        return this.nodeStat.median
-    }
-
-    get repartitionNodeLength (): Map<number, number> {
-        return this.nodeStat.lengthRepartition
-    }
-
-    get repartitionNodeLengthString (): string {
-        const arr = Array.from(this.nodeStat.lengthRepartition)
-        arr.sort((a, b) => {
-            return a[0] - b[0]
-        })
-        let str = ""
-        arr.forEach((entry) => {
-            str += "(" + entry[0] + ", " + entry[1] + "), "
-        })
-        return str
-    }
-
-    get maxIdentifierLength (): number {
-        return this.identifierStat.max
-    }
-
-    get minIdentifierLength (): number {
-        return this.identifierStat.min
-    }
-
-    get meanIdentifierLength (): number {
-        return this.identifierStat.mean
-    }
-
-    get medianIdentifierLength (): number {
-        return this.identifierStat.median
-    }
-
-    get repartitionIdentifierLength (): Map<number, number> {
-        return this.identifierStat.lengthRepartition
-    }
-
-    get repartitionIdentifierLengthString (): string {
-        const arr = Array.from(this.identifierStat.lengthRepartition)
-        arr.sort((a, b) => {
-            return a[0] - b[0]
-        })
-        let str = ""
-        arr.forEach((entry) => {
-            str += "(" + entry[0] + ", " + entry[1] + "), "
-        })
-        return str
     }
 }
