b3ca2dfc580365d51cfdd5dc32572ce3e0ce2f9a
Matthieu Nicolas <nicolasmatthieu57@gmail.com>
2019-01-28 16:38:37 +0100

    feat(renamingmapstore): add fromPlain()

--- src/renamingmap/renamingmapstore.ts
+++ src/renamingmap/renamingmapstore.ts
@@ -17,14 +17,39 @@
     along with this program.  If not, see <https://www.gnu.org/licenses/>.
 */
 
-import {Epoch} from "../epoch/epoch"
-import {EpochId} from "../epoch/epochid"
-import {IdentifierInterval} from "../identifierinterval"
-import {LogootSRename} from "../operations/rename/logootsrename"
-import {ExtendedRenamingMap} from "./extendedrenamingmap"
-import {RenamingMap} from "./renamingmap"
+import { isArrayFromMap, isObject } from "../data-validation"
+import { Epoch } from "../epoch/epoch"
+import { EpochId } from "../epoch/epochid"
+import { ExtendedRenamingMap } from "./extendedrenamingmap"
+import { RenamingMap } from "./renamingmap"
+
+export interface RenamingMapStoreJSON {
+    readonly renamingMaps: Array<[string, RenamingMap]>
+}
 
 export class RenamingMapStore {
+
+    static fromPlain (o: unknown): RenamingMapStore | null {
+        if (isObject<RenamingMapStoreJSON>(o) &&
+            Array.isArray(o.renamingMaps) && o.renamingMaps.length > 0) {
+
+            const renamingMaps = o.renamingMaps
+                .filter(isArrayFromMap)
+                .map(([k, v]): [unknown, RenamingMap | null] => {
+                    return [k, RenamingMap.fromPlain(v)]
+                })
+                .filter((arg): arg is [string, RenamingMap] => typeof arg[0] === "string" && arg [1] !== null)
+
+            if (o.renamingMaps.length === renamingMaps.length) {
+                const renamingMapStore = new RenamingMapStore()
+                renamingMaps.forEach(([epochId, renamingMap]) => {
+                    renamingMapStore.internalAdd(epochId, renamingMap)
+                })
+                return renamingMapStore
+            }
+        }
+        return null
+    }
 
     private renamingMaps: Map<string, RenamingMap>
 
@@ -33,7 +58,7 @@ export class RenamingMapStore {
     }
 
     add (epoch: Epoch, renamingMap: RenamingMap) {
-        this.renamingMaps.set(epoch.id.asStr, renamingMap)
+        this.internalAdd(epoch.id.asStr, renamingMap)
     }
 
     getExtendedRenamingMap (epochId: EpochId): ExtendedRenamingMap | undefined {
@@ -42,5 +67,13 @@ export class RenamingMapStore {
             return ExtendedRenamingMap.fromRenamingMap(renamingMap)
         }
         return undefined
+    }
+
+    toJSON (): RenamingMapStoreJSON {
+        return { renamingMaps: Array.from(this.renamingMaps) }
+    }
+
+    private internalAdd (epochId: string, renamingMap: RenamingMap) {
+        this.renamingMaps.set(epochId, renamingMap)
     }
 }
